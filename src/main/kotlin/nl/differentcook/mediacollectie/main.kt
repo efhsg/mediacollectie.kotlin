4f43f99f466e5169cfd41ae583089b0ae3980dd9
Erwin Sollewijn Gelpke <efhsg@live.nl>
2018-03-03 00:53:29 +0100

    Database access 0.1

--- src/main/kotlin/nl/differentcook/mediacollectie/main.kt
+++ src/main/kotlin/nl/differentcook/mediacollectie/main.kt
@@ -1,6 +1,22 @@
 package nl.differentcook.mediacollectie
 
+import org.jetbrains.exposed.dao.IntIdTable
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+
 fun main(args: Array<String>) {
 
+    Database.connect("jdbc:mysql://192.168.10.10:3306/mediacollectie", driver = "com.mysql.jdbc.Driver", user = "homestead", password = "secret")
 
+    transaction {
+        logger.addLogger(StdOutSqlLogger)
+        println("Schijven: ${Schijven.selectAll()}")
+    }
+}
+
+object Schijven : IntIdTable() {
+    val naam = varchar("naam", 50)
+    val capaciteit = varchar("capaciteit", 50)
 }
