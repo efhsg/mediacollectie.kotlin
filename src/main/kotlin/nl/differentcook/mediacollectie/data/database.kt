262b41f4f4b0bf78980ead858c66a5fdb2c9dca8
Erwin Sollewijn Gelpke <efhsg@live.nl>
2018-03-06 00:27:36 +0100

    Query 1.0.7 refactor

--- src/main/kotlin/nl/differentcook/mediacollectie/data/database.kt
+++ src/main/kotlin/nl/differentcook/mediacollectie/data/database.kt
@@ -1,8 +1,7 @@
 package nl.differentcook.mediacollectie.data
 
 import io.github.cdimascio.dotenv.dotenv
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.Database
 
 fun getDatabase() {
     val dotenv = dotenv {
@@ -11,60 +10,4 @@ fun getDatabase() {
 
     Database.connect(dotenv["DB_URL"] ?: "", driver = dotenv["DB_DRIVER"] ?: "",
             user = dotenv["DB_USER"] ?: "", password = dotenv["DB_PASSWORD"] ?: "")
-}
-
-object Schijven : Table(name = "schijven") {
-    val id = integer("id").primaryKey()
-    val naam = varchar("naam", 25).uniqueIndex()
-    val capaciteit = integer("capaciteit")
-    val beschikbaar = integer("beschikbaar")
-    val scandatum = date("scandatum")
-    val created_at = date("created_at")
-    val updated_at = date("updated_at")
-}
-
-object Mappen : Table(name = "mappen") {
-    val id = integer("id").primaryKey()
-    val naam = varchar("naam", 200)
-    val created_at = date("created_at")
-    val updated_at = date("updated_at")
-}
-
-object Bestanden : Table(name = "bestanden") {
-    val id = integer("id").primaryKey()
-    val naam = varchar("naam", 150)
-    val bestandstype = varchar("bestandstype", 25) references Bestandstypes.naam
-    val map = integer("map") references Mappen.id
-    val schijf = varchar("schijf", 25) references Schijven.naam
-    val grootte = integer("grootte")
-    val created_at = date("created_at")
-    val updated_at = date("updated_at")
-}
-
-object Bestandstypes : Table(name = "bestandstypes") {
-    val id = integer("id").primaryKey()
-    val naam = varchar("naam", 25).uniqueIndex()
-    val created_at = date("created_at")
-    val updated_at = date("updated_at")
-}
-
-object Ondertitels : Table(name = "ondertitels") {
-    val id = integer("id").primaryKey()
-    val bestand = integer("bestand") references Bestanden.id
-    val taal = varchar("taal", 25)
-    val soort = varchar("soort", 25)
-    val created_at = date("created_at")
-    val updated_at = date("updated_at")
-}
-
-fun queryBestanden(zoekNaam: String?, function: (ResultRow) -> Unit) {
-    val join = Bestanden innerJoin Mappen
-    getDatabase()
-    transaction {
-        logger.addLogger(StdOutSqlLogger)
-        val query = if (zoekNaam != null)
-            (join).select { Bestanden.naam like "%" + zoekNaam + "%" } else
-            (join).selectAll()
-        query.forEach { it -> function(it) }
-    }
 }
