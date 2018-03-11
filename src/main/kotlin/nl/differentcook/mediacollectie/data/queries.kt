7271305fadaf7c88c7c74053de90207da27d7945
Erwin Sollewijn Gelpke <efhsg@live.nl>
2018-03-11 23:02:48 +0100

    Query 1.1.5 - Move4 `found` from query to lambda - Units test more than one record

--- src/main/kotlin/nl/differentcook/mediacollectie/data/queries.kt
+++ src/main/kotlin/nl/differentcook/mediacollectie/data/queries.kt
@@ -1,21 +1,17 @@
 package nl.differentcook.mediacollectie.data
 
 import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
-fun queryBestanden(zoekNaam: String?, function: (ResultRow) -> Unit): MutableList<Int> {
+fun queryBestanden(zoekNaam: String?, function: (ResultRow) -> Unit) {
     getDatabase()
     val join = Bestanden innerJoin Mappen
-    val found: MutableList<Int> = mutableListOf()
     transaction {
         val query = if (zoekNaam != null)
             (join).select { Bestanden.naam like "%" + zoekNaam + "%" } else
             (join).selectAll()
-        query.forEach({ it -> found.add(it[Bestanden.id]) })
-        query.forEach { it -> function(it) }
+        query.forEach { resultRow -> function(resultRow) }
     }
-    return found
 }
--- src/test/kotlin/nl/differentcook/mediacollectie/query/QueryTests.kt
+++ src/test/kotlin/nl/differentcook/mediacollectie/query/QueryTests.kt
@@ -19,12 +19,14 @@ class QueryTests {
     @Test
     fun `query on Prospero's Books movie`() {
         val movieName = "Prospero's Books"
-        val found = queryBestanden(movieName,
+        val found: MutableList<Int> = mutableListOf()
+        queryBestanden(movieName,
                 {
                     assert(it[Bestanden.schijf].equals("2016"))
-                    assert(it[Mappen.naam].equals("\\Film\\Klassieker\\"))
+                    assert(it[Mappen.naam].equals("\\Film\\Klassieker"))
                     assert(it[Bestanden.naam].equals(movieName))
                     assert(it[Bestanden.bestandstype].equals("avi"))
+                    found.add(it[Bestanden.id])
                 }
         )
         assert(found.size == 1)
@@ -35,13 +37,26 @@ class QueryTests {
     @Test
     fun `query on n'existe pas`() {
         val movieName = "n'existe pas"
-        val found = queryBestanden(movieName,
+        queryBestanden(movieName,
                 {
                     assert(false)
                 }
         )
-        assert(found.isEmpty())
     }
 
+    @Test
+    fun `query on Godfather movies`() {
+        val movieName = "Godfather"
+        val found: MutableList<Int> = mutableListOf()
+        queryBestanden(movieName,
+                {
+                    found.add(it[Bestanden.id])
+                }
+        )
+        assert(found.size == 3)
+        assert(found.contains(2))
+        assert(found.contains(3))
+        assert(found.contains(4))
+    }
 
 }
