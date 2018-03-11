5d96ebb8ff1f07306f93e2545805f6cf07a7135a
Erwin Sollewijn Gelpke <efhsg@live.nl>
2018-03-11 23:07:11 +0100

    Query 1.1.6 - Added records found to printBestanden()

--- src/main/kotlin/nl/differentcook/mediacollectie/query/main.kt
+++ src/main/kotlin/nl/differentcook/mediacollectie/query/main.kt
@@ -11,11 +11,14 @@ fun main(args: Array<String>) {
 }
 
 private fun printBestanden(zoekTerm: String?) {
+    val found: MutableList<Int> = mutableListOf()
     queryBestanden(zoekTerm,
             {
                 println("${it[Bestanden.schijf]}:${it[Mappen.naam]}\\${it[Bestanden.naam]}.${it[Bestanden.bestandstype]}")
+                found.add(it[Bestanden.id])
             }
     )
+    println("Records found: $found")
 }
 
 private fun printStreep() {
